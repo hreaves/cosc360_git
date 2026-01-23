12d630220ee8c02f68ac9950011af9aa1dae02c2
Harrison Cade Reaves <hreaves1@tesla18.eecs.utk.edu>
2026-01-23 09:46:50 -0500

    Initial Lab1 files

---
+++ Lab-1-Chain-Heal/chain_heal.c
@@ -0,0 +1,10 @@
+// COSC360 Lab1 Chain Heal
+// Harrison Reaves
+// Using DFS to get an optimal healing path
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+
