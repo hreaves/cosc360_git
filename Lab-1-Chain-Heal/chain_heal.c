9eaa4e306498055e866e5d033b93f3dfecc7e8ef
Harrison Cade Reaves <hreaves1@tesla18.eecs.utk.edu>
2026-01-24 16:04:28 -0500

    read all nodes into array

--- Lab-1-Chain-Heal/chain_heal.c
+++ Lab-1-Chain-Heal/chain_heal.c
@@ -8,3 +8,47 @@
 #include <math.h>
 
 
+typedef struct node {
+  char *name;
+  int x, y;
+  int cur_PP, max_PP;
+  struct node *prev;
+} Node;
+
+
+int main(int argc, char **argv) {
+	int initial_range, jump_range, num_jumps, initial_power;
+	double power_reduction;
+
+	initial_range = atoi(argv[1]);
+	jump_range = atoi(argv[2]);
+	num_jumps = atoi(argv[3]);
+	initial_power = atoi(argv[4]);
+	power_reduction = atof(argv[5]);
+
+	char name[100];
+	int x, y, cur_PP, max_PP, num_nodes = 0;
+	Node *head = NULL, *prev = NULL;
+	while(scanf("%d %d %d %d %99s", &x, &y, &cur_PP, &max_PP, name)==5) { 
+		Node *n;
+		n = (Node *) malloc(sizeof(Node));
+		n->x = x;
+		n->y = y;
+		n->cur_PP = cur_PP;
+		n->max_PP = max_PP;
+		n->name = name;
+		num_nodes++;
+		n->prev = prev;
+		prev = n;
+		head = n;
+	}
+
+	Node **nodes;
+	nodes = (Node **) malloc(num_nodes * (sizeof(Node*)));
+	for(int i = 0; i < num_nodes; i++) {
+		nodes[i] = head;
+		head = head->prev;
+	}
+
+
+return 0; }
