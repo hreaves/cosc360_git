c0e7343282a10cf2e1b0a95c4e733a70a5a40040
Harrison Cade Reaves <hreaves1@tesla18.eecs.utk.edu>
2026-01-24 20:09:09 -0500

    finished graph

--- Lab-1-Chain-Heal/chain_heal.c
+++ Lab-1-Chain-Heal/chain_heal.c
@@ -9,11 +9,17 @@
 
 
 typedef struct node {
-  char *name;
-  int x, y;
-  int cur_PP, max_PP;
-  struct node *prev;
+	char *name;
+	int x, y;
+	int cur_PP, max_PP;
+	struct node *prev;
+	int adj_size;
+	struct node **adj;
 } Node;
+
+//void DFS(Node *n, int hop_num,// ) {
+
+//}
 
 
 int main(int argc, char **argv) {
@@ -29,14 +35,15 @@ int main(int argc, char **argv) {
 	char name[100];
 	int x, y, cur_PP, max_PP, num_nodes = 0;
 	Node *head = NULL, *prev = NULL;
-	while(scanf("%d %d %d %d %99s", &x, &y, &cur_PP, &max_PP, name)==5) { 
+	while(scanf("%d %d %d %d %99s", &x, &y, &cur_PP, &max_PP, name) == 5) { 
 		Node *n;
 		n = (Node *) malloc(sizeof(Node));
 		n->x = x;
 		n->y = y;
 		n->cur_PP = cur_PP;
 		n->max_PP = max_PP;
-		n->name = name;
+		n->name = (char *) malloc(100 * sizeof(char));
+		strcpy(n->name, name);
 		num_nodes++;
 		n->prev = prev;
 		prev = n;
@@ -49,6 +56,37 @@ int main(int argc, char **argv) {
 		nodes[i] = head;
 		head = head->prev;
 	}
-
+	// calculate number of adjacenies
+	int x_dist, y_dist;
+	for(int i = 0; i < num_nodes; i++) {
+		nodes[i]->adj_size = 0;
+		for(int j = 0; j < num_nodes; j++) {
+			if(i==j) {
+				continue;
+			}
+			x_dist = nodes[i]->x - nodes[j]->x;
+			y_dist = nodes[i]->y - nodes[j]->y;
+			if((x_dist*x_dist + y_dist*y_dist) <= (jump_range*jump_range)) {
+				nodes[i]->adj_size++;
+			}
+		}
+		nodes[i]->adj = (Node **) malloc(nodes[i]->adj_size * sizeof(Node*));
+	}
+	// fill up the adjacency lists
+	int k;
+	for(int i = 0; i < num_nodes; i++) {
+		k = 0;
+        for(int j = 0; j < num_nodes; j++) {
+			if(i==j) {
+				continue;
+			}
+            x_dist = nodes[i]->x - nodes[j]->x;
+            y_dist = nodes[i]->y - nodes[j]->y;
+			if((x_dist*x_dist + y_dist*y_dist) <= (jump_range*jump_range)) {
+                nodes[i]->adj[k] = nodes[j];
+				k++;
+			}
+		}
+	}
 
 return 0; }
