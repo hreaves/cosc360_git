9315907d43a96d33ea264ecf2a471692b2cb67a1
Harrison Cade Reaves <hreaves1@tesla10.eecs.utk.edu>
2026-02-27 14:12:21 -0500

    initial Lab 6 files

---
+++ Lab-6-Tarx/src/tarx.c
@@ -0,0 +1,17 @@
+/*  COSC360 Lab6 TARX
+	Harrison Reaves
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include "dllist.h"
+#include "jval.h"
+#include "jrb.h"
+
+int main() {
+
+	return 0;
+}
