0a3a637330de393a36af8fa585ef87b5e889275c
Harrison Cade Reaves <hreaves1@tesla10.eecs.utk.edu>
2026-03-23 17:21:53 -0400

    comments added

--- Lab-6-Tarx/src/tarx.c
+++ Lab-6-Tarx/src/tarx.c
@@ -1,6 +1,8 @@
 /*  COSC360 Lab6 TARX
 	Harrison Reaves
-
+	This program recreates a directory from a tarfile that was created with tarc,
+	including all file and directory protections and modification times. It reads
+	the tarfile from standard input.
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,7 +27,7 @@ int compare(Jval v1, Jval v2) {
 	if (v1.l > v2.l) return 1;
 	return 0;
 }
-
+// Function to clear all memory before exiting
 void clean(Dllist times, JRB inodes, char *fn) {
 	Dllist tmp;
 	dll_traverse(tmp, times) {
@@ -44,24 +46,21 @@ int main() {
 	int fn_size, read_bytes, fn_size_check;
 	char *fn;
 
-	char command[1000];
-	char block[4096];
+	char block[4096]; // buffer for reading/writing
 
-	JRB inodes;
+	JRB inodes; // all seen inodes
 	inodes = make_jrb();
-	Dllist times, tmp;
+	Dllist times, tmp; // holds data struct for every inode to chmod later
 	times = new_dllist();
 
-	struct timeval t[2];
-
+	struct timeval t[2]; 
+	// read through all of stdin, check for errors every time using fread
 	while ((fn_size_check = fread(&fn_size, 1, 4, stdin)) != 0) {
-
 		if (fn_size_check != 4) {
 			fprintf(stderr, "Bad tarfile\n");
 			clean(times, inodes, fn);
 			exit(1);
 		}
-
 		fn = (char *) malloc(fn_size + 1);
 		if (fread(fn, 1, fn_size, stdin) != fn_size) {
 			fprintf(stderr, "Bad tarfile\n"); 
@@ -74,24 +73,25 @@ int main() {
 			clean(times, inodes, fn);
 			exit(1);
 		}
+		// check if inode has already been read, if not, create data struct and add to JRB tree
 		if (jrb_find_gen(inodes, new_jval_l(f_inode), compare) == NULL) {
 			Data *data = malloc(sizeof(Data));
             data->name = strdup(fn);
 			jrb_insert_gen(inodes, new_jval_l(f_inode), new_jval_v(data), compare); 
+			// since its new inode, you need mtime and mode as well
 			if (fread(&data->mode, 1, 4, stdin) != 4) {
 				fprintf(stderr, "Bad tarfile\n");
 				clean(times, inodes, fn);
 				exit(1);
 			}
-
 			if (fread(&data->mtime, 1, 8, stdin) != 8) {
 				fprintf(stderr, "Bad tarfile\n"); 
 				clean(times, inodes, fn);
 				exit(1);
 			}
-
+			// add struct to dllist for later
 			dll_append(times, new_jval_v(data));
-
+			// if its regular file, then we need to read f_size and bytes of file
 			if (S_ISREG(data->mode)) {
 				if (fread(&f_size, 1, 8, stdin) != 8) {
 					fprintf(stderr, "Bad tarfile\n");
@@ -100,38 +100,43 @@ int main() {
 				}
 				FILE *fp = fopen(fn, "wb");
 				byte_count = 0;
+				// read all bytes from stdin with blocks of 4096
 				while(byte_count < f_size) {
 					read_bytes = 4096;
+					// last block
 					if(f_size - byte_count < 4096) {
 						read_bytes = f_size - byte_count;
-					}	
+					}
 					if (fread(block, 1, read_bytes, stdin) != read_bytes) {
 						fprintf(stderr, "Bad tarfile\n");
 						clean(times, inodes, fn);
 						fclose(fp);
 						exit(1);
 					}
+					// write to the newly opened file
 					fwrite(block, 1, read_bytes, fp);
 					byte_count += read_bytes;
 				}
 				fclose(fp);
 			}
+			// if not regular file, its a directory, so mkdir the name
 			else {
 				mkdir(fn, 0700);				
 			}
 		}
+		// if its not a new inode, create a hard link with the current filename and the already existant filename
 		else {
 			JRB node = jrb_find_gen(inodes, new_jval_l(f_inode), compare);
             link(((Data *)node->val.v)->name, fn);
 		}
 		free(fn);
 	}
-
+	// Editing metadata for each inode and then changing permissions. AI helped me with this.
 	dll_rtraverse(tmp, times) {
 		Data *data = (Data *) tmp->val.v;
-		t[0].tv_sec = time(NULL); // Access time (atime)
+		t[0].tv_sec = time(NULL); // Access time
 		t[0].tv_usec = 0;
-		t[1].tv_sec = data->mtime;   // Modification time (mtime)
+		t[1].tv_sec = data->mtime; // Modification time
 		t[1].tv_usec = 0;
 		utimes(data->name, t);
 		chmod(data->name, data->mode);
